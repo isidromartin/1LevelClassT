166cee0156afad25cf909e3cd7a6b6e205d5b5c9
Isidro <i.martin@pristok.com>
2025-02-26 20:07:15 +0100

    edit ghpages4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/1LevelClass/",
+  base: "/1LevelClassT/",
   plugins: [react()],
 });
